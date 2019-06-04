1d279b880079cf0808cd8f77b8b485e41f73c609
erickeno <erickeno12@gmail.com>
2019-06-04 15:30:31 -0400

    update configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import logo from "./images/logo.svg";
+import * as React from 'react';
+import logo from './images/logo.svg';
 
 const App: React.FC = () => {
   return (
