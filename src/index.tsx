35df2a3216f59aa505b69441a9c1b85092b845b8
erickeno <erickeno12@gmail.com>
2019-06-04 23:24:47 -0400

    include amplify as a serverless GraphQL backend

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import AWSAppSyncClient, { AUTH_TYPE } from 'aws-appsync';
+import { ApolloProvider } from 'react-apollo';
+import Amplify from 'aws-amplify';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './serviceWorker';
+import './index.css';
+import config from './aws-exports';
+
+Amplify.configure(config);
+
+const client = new AWSAppSyncClient({
+  disableOffline: true,
+  url: config.aws_appsync_graphqlEndpoint,
+  region: config.aws_appsync_region,
+  auth: {
+    type: AUTH_TYPE.API_KEY,
+    apiKey: config.aws_appsync_apiKey,
+  },
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
 serviceWorker.unregister();
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,13 @@ import * as React from 'react';
 import { RouteComponentProps, Link } from '@reach/router';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import { Button } from '../components/globals/Button';
 import { Layout } from '../layout';
+import { listPosts } from '../graphql/queries';
+import { ListPostsQuery, ListPostsQueryVariables } from '../API';
 
 export const Home: React.FC<RouteComponentProps> = () => {
   return (
@@ -16,6 +20,30 @@ export const Home: React.FC<RouteComponentProps> = () => {
         <Typography variant="h4">Welcome to CRA Boilerplate</Typography>
       </Box>
       <Button>Hello world</Button>
+      <Typography variant="h4">Blog Posts</Typography>
+      <Query<ListPostsQuery, ListPostsQueryVariables>
+        query={gql(listPosts)}
+        variables={{ limit: 100 }}
+      >
+        {({ loading, error, data }) => {
+          if (loading) {
+            return <p>loading</p>;
+          }
+          if (error) {
+            return <p>Error :(</p>;
+          }
+          if (!data) {
+            return null;
+          }
+          if (data && data.listPosts && data.listPosts.items) {
+            return data.listPosts.items.map((row, index) => (
+              <Typography key={index} variant="body1">
+                {row && row.title}
+              </Typography>
+            ));
+          }
+        }}
+      </Query>
     </Layout>
   );
 };
